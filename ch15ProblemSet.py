33c6b2f45eebd6d2a78cde29348a9c665e244ded
nsachs <nsachs@fwparker.org>
2017-02-20 13:55:06 -0600

    progress

--- ch15ProblemSet.py
+++ ch15ProblemSet.py
@@ -6,11 +6,25 @@ from class and from Ch15 of the textbook website
 #1.  (7pts) Write code which finds and prints the longest
 # word in the provided dictionary.  If there are more
 # than one longest word, print them all.
+import re
+file = open("dictionary.txt", "r")
+text_list = []
+
+for word in file:
+    if (len(word)) >= 25:
+        print(word)
+
+
 
 #2.  (10pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
+'''
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+    line_list.append(line)
 
+'''
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
 #3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
