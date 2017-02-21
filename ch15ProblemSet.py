08844c650e44e485843a32d1ed064302bfc3189f
nsachs <nsachs@fwparker.org>
2017-02-21 14:10:32 -0600

    progress

--- ch15ProblemSet.py
+++ ch15ProblemSet.py
@@ -8,9 +8,7 @@ from class and from Ch15 of the textbook website
 # than one longest word, print them all.
 import re
 file = open("dictionary.txt", "r")
-text_list = []
 highest = 0
-
 
 for word in file:
     if (len(word)) >= highest:
@@ -22,12 +20,28 @@ print(longest_word)
 #2.  (10pts)  Write code which finds
 # The total word count AND average word length
 # in "AliceInWonderLand.txt"
-'''
+
+import re
+#Number of words in text
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
-    line_list.append(line)
 
-'''
+file = open("AliceInWonderLand.txt")
+text_list = []
+
+for line in file:
+    words = split_line(line)
+    for word in range(len(words)):
+        text_list.append(words[word])
+print("There are", len(text_list), "words in Alice In Wonderland.")
+
+#Average length of words in text
+for i in range(len(text_list)):
+    word_length = len(text_list[i])
+average_length = word_length / len(text_list)
+print("The average word length is", average_length, "letters.")
+
+
 # CHOOSE ONE OF THE FOLLOWING TWO PROBLEMS
 
 #3 (13pts)  How many times does "Cheshire" occur in"AliceInWonderLand.txt"?
@@ -38,6 +52,20 @@ def split_line(line):
 
 #3  (13pts)Find the most frequently occurring
 # seven letter word in "AliceInWonderLand.txt"
+
+def split_line(line):
+    return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
+
+file = open("AliceInWonderLand.txt")
+text_list = []
+
+for line in file:
+    words = split_line(line)
+    for word in range(len(words)):
+        text_list.append(words[word])
+
+print("There are", len(text_list), "words with seven letters in Alice In Wonderland!")
+
 
 # Challenge problem (for fun).  What words appear in the text of "Alice in Wonderland" that DO NOT occur in "Alice Through the Looking Glass".  Make a list.  You can substitute this for any of the above problems.
 
